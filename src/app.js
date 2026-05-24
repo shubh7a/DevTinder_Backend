fcb52e2bb5bf60b94966d74320e8841da9fd6f04
Shubham Kumar Singh <shubh7a@gmail.com>
2026-05-24 15:11:31 +0530

    deployment ready

--- src/app.js
+++ src/app.js
@@ -85,6 +85,11 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
+res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none"
+});
 connectDb()
   .then(() => {
     console.log("Database Connection Established");
